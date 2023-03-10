149463b6ef70c9e8864c1aa2ebb160f91ee7d348
MandeepMann <mandeep_mann30@hotmail.com>
2023-03-10 16:43:05 -0500

    started up basic layout and styles for footer

---
+++ src/Footer.js
@@ -0,0 +1,43 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="wrapper">
+                <div className="footerContent">
+                    <div className="footerLinks">
+                        <div className="footerImgContainer">
+                            <img src="https://placebear.com/100/50" alt="placeholder of a brown bear" />
+                        </div>
+
+                        <div className="footerUl">
+                            <ul className="socialLinks">
+                                <li><a href="#">Facebook</a></li>
+                                <li><a href="#">Twitter</a></li>
+                                <li><a href="#">Instagram</a></li>
+                                <li><a href="#">Whatsapp</a></li>
+                            </ul>
+
+                            <ul className="contactLinks">
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, ratione quo? Aut iure mollitia deleniti. Debitis reprehenderit cum quae hic facilis. Distinctio deserunt minus ducimus dignissimos. Voluptatibus dolore eum quis.</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
