3470e600806dc71cf5304dd217903ae109a3b2fe
MandeepMann <mandeep_mann30@hotmail.com>
2023-03-10 17:09:43 -0500

    basic html and styles done for blog list section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import BlogList from './BlogList.js';
 import Footer from './Footer.js';
+
+
 
 function App() {
   return (
     <div className="App">
+        <BlogList />
         <Footer />
     </div>
   );
---
+++ src/BlogList.js
@@ -0,0 +1,74 @@
+const BlogList =  () => {
+    return (
+        <section className="blogListSection">
+            <div className="wrapper">
+                <h3>Coluptate velit esse cillum</h3>
+
+                <div className="blogList">
+                    <div className="blogRow">
+                        <div className="blogInfo">
+                            <div className="blogImgContainer">
+                                <img src="https://placebear.com/200/200" alt="placeholder of a brown bear in water" />
+                            </div>
+                            <div className="blogText">
+                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                                <a href="">Blog Post 1</a>
+                            </div>
+                        </div>
+                        <div className="blogInfo">
+                            <div className="blogImgContainer">
+                                <img src="https://placebear.com/200/200" alt="placeholder of a brown bear in water" />
+                            </div>
+                            <div className="blogText">
+                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                                <a href="">Blog Post 2</a>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className="blogRow">
+                        <div className="blogInfo">
+                            <div className="blogImgContainer">
+                                <img src="https://placebear.com/200/200" alt="placeholder of a brown bear in water" />
+                            </div>
+                            <div className="blogText">
+                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                                <a href="">Blog Post 3</a>
+                            </div>
+                        </div>
+                        <div className="blogInfo">
+                            <div className="blogImgContainer">
+                                <img src="https://placebear.com/200/200" alt="placeholder of a brown bear in water" />
+                            </div>
+                            <div className="blogText">
+                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                                <a href="">Blog Post 4</a>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className="blogRow">
+                        <div className="blogInfo">
+                            <div className="blogImgContainer">
+                                <img src="https://placebear.com/200/200" alt="placeholder of a brown bear in water" />
+                            </div>
+                            <div className="blogText">
+                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                                <a href="">Blog Post 5</a>
+                            </div>
+                        </div>
+                        <div className="blogInfo">
+                            <div className="blogImgContainer">
+                                <img src="https://placebear.com/200/200" alt="placeholder of a brown bear in water" />
+                            </div>
+                            <div className="blogText">
+                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p>
+                                <a href="">Blog Post 6</a>
+                            </div>
+                        </div>                        
+                    </div>                                                       
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default BlogList;
