1c96a054595dadef5f63209481841479e7ef5ff6
MandeepMann <mandeep_mann30@hotmail.com>
2023-03-12 16:43:46 -0400

    fixed more noreferrer links :hand_over_mouth:

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -16,16 +16,16 @@ const NavMenu = () => {
                     </div>
                     <ul className="navLinks">
                         <Link to="/">Home</Link>
-                        <li><a href="https://github.com/MandeepKMann/mockup" target="_blank" rel="norefferer">GitHub</a></li>
-                        <li><a href="https://www.linkedin.com/in/mandeepkmann/" target="_blank" rel="norefferer">LinkedIn</a></li>
-                        <li><a href="https://findtheinvisiblecow.com/" target="_blank" rel="norefferer">Find the Cow</a></li>
-                        <li><a href="https://explore.org/livecams/cats/kitten-rescue-cam" target="_blank" rel="norefferer">Kitten Cam</a></li>
+                        <li><a href="https://github.com/MandeepKMann/mockup" target="_blank" rel="noreferrer">GitHub</a></li>
+                        <li><a href="https://www.linkedin.com/in/mandeepkmann/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+                        <li><a href="https://findtheinvisiblecow.com/" target="_blank" rel="noreferrer">Find the Cow</a></li>
+                        <li><a href="https://explore.org/livecams/cats/kitten-rescue-cam" target="_blank" rel="noreferrer">Kitten Cam</a></li>
                     </ul>
                 </div>
 
                 <div className="loginServices navRight">
                     <button>Subscribe</button>
-                    <a href="https://github.com/MandeepKMann/mockup" target="_blank" rel="norefferer">Sign in</a>
+                    <a href="https://github.com/MandeepKMann/mockup" target="_blank" rel="noreferrer">Sign in</a>
                 </div>
             </div>
         </nav>
