15417bb1851df9eb2bae86751d2dfa6fc85055c7
MandeepMann <mandeep_mann30@hotmail.com>
2023-03-12 14:09:59 -0400

    figured out js for carousel! :tada:

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,40 @@
+import { useState } from "react";
+import bears from '../carouselContent'
+
 const Carousel = ({images}) => {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const slideLeft = () => {
+        if (currentSlide === 0) {
+            setCurrentSlide(bears.length - 1)
+        } else {
+            setCurrentSlide(currentSlide - 1)
+        }
+    }
+
+    const slideRight = () => {
+        if (currentSlide === (bears.length - 1)) {
+            setCurrentSlide(0)
+        } else {
+            setCurrentSlide(currentSlide + 1)
+        }
+    }
+
     return (
         <section className="carouselSection">
             <div className="carousel">
                 <div className="carouselWrapper wrapper">
                     {images.map((image, index) => {
                         return (
-                            <div key={index} className={index == 2 ? "carouselCard carouselCard--active" : "carouselCard"}>
+                            <div 
+                                key={index} 
+                                className={
+                                    index == currentSlide 
+                                    ? "carouselCard carouselCard--active" 
+                                    : "carouselCard"
+                                }
+                            >
                                 <img src={image.image} alt={image.alt} />
                                 <div className="cardOverlay">
                                     <h2 className="cardTitle">{image.title}</h2>
@@ -13,6 +42,8 @@ const Carousel = ({images}) => {
                             </div>
                         )
                     })}
+                    <div className="carouselArrowLeft" onClick={slideLeft}>&lsaquo;</div>
+                    <div className="carouselArrowRight" onClick={slideRight}>&rsaquo;</div>
                 </div>
             </div>
         </section>
